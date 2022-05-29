0a677fc1f78c812b98dab0a5359a162dc12886e5
PraneelChugh <86180715+PraneelChugh@users.noreply.github.com>
2022-05-29 12:29:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,6 +24,9 @@ function setup(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        confirm.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristx = " + leftWristX + "leftWristY = " + leftWristY );
@@ -40,6 +43,17 @@ function modelLoaded(){
 
 function draw(){
     image(video,0,0,600,500);
+    fill('#FF0000');
+    stroke('#FF0000');
+    circle(leftWristX,leftWristY,20);
+
+    if(scoreLeftWrist > 0.2){
+    InNumberleftWristY = Number(leftWristY) ;
+    remove_decimals= floor( InNumberleftWristY);
+    volume = remove_decimals/500; 
+    document.getElementById("volume").innerHTML = "Volume =" + volume;
+    song.setVolum(volume);
+}
 }
 
 function play(){
